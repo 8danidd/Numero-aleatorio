1c086f29a41fb78593758a9dec1774f384f573d4
Daniel . <8danidd@gmail.com>
2024-10-28 00:03:42 +0100

    Bucle continuo del juego

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
 
- public class Suerte
-{ 
+public class Suerte
+{
     public static void Main(string[] args)
     {
-        static void Juego()
+        while (true)
         {
-            int ciclos = 1;
-            int randomnum = -1;
-            Console.WriteLine("Introduce un numero del 1 al 1000");
-            int suerte = Convert.ToInt32(Console.ReadLine());
-            while (true)
-            {
-                Thread.Sleep(1);
-                Random random = new Random();
-                randomnum = random.Next(1, 1001);
-                Console.WriteLine(ciclos + ": " + randomnum);
-                ciclos = ciclos + 1;
-                if (randomnum == suerte)
-                {
-                    break;
-                }
-            }
-            Console.WriteLine("\n\nHas generado " + (ciclos - 1) + " numeros aleatorios para conseguir el " + suerte + ".\n");
+            Juego();
         }
-        Juego();
+    }
+    static void Juego()
+    {
+        int ciclos = 1;
+        int randomnum = -1;
+        Console.WriteLine("Introduce un numero del 1 al 1000");
+        int suerte = Convert.ToInt32(Console.ReadLine());
+        while (true)
+        {
+            Thread.Sleep(1);
+            Random random = new Random();
+            randomnum = random.Next(1, 1001);
+            Console.WriteLine(ciclos + ": " + randomnum);
+            ciclos = ciclos + 1;
+            if (randomnum == suerte)
+            {
+                break;
+            }
+        }
+        Console.WriteLine("\n\nHas generado " + (ciclos - 1) + " numeros aleatorios para conseguir el " + suerte + ".\n\n");
+        Thread.Sleep(500);
     }
 }
