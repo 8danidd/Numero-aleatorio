9457c8c86ece642a8ce6793b42372ad0e3822af2
danidd <danidd@MSI-P>
2024-10-27 21:59:16 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+ public class Suerte
+{ 
+    public static void Main(string[] args)
+    {
+        static void Juego()
+        {
+            int ciclos = 1;
+            int randomnum = -1;
+            Console.WriteLine("Introduce un numero del 0 al 1000");
+            int suerte = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                Thread.Sleep(1);
+                Random random = new Random();
+                randomnum = random.Next(1, 1001);
+                Console.WriteLine(ciclos + ": " + randomnum);
+                ciclos = ciclos + 1;
+                if (randomnum == suerte)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\n\nHas generado " + (ciclos - 1) + " numeros aleatorios para conseguir el " + suerte + ".\n");
+        }
+        Juego();
+    }
+}
